aae0e99ab9325e9b71144d9ed8369da24e78e29d
Mark de Dios <mhdedios@gmail.com>
2017-06-03 12:13:27 -0700

    Finish almost code for vampires.rb

---
+++ ruby/vampires.rb
@@ -0,0 +1,50 @@
+puts "How many employees will be processed?"
+number_processed = gets.chomp.to_i
+
+while number_processed > 0 do
+  puts "What is your name?"
+  employee = gets.chomp
+
+  puts "How old are you?"
+  age = gets.chomp.to_i
+
+  puts "What year were you born?"
+  year = gets.chomp.to_i
+
+  puts "Our company cafeteria serves garlic bread. Should we order some for you? Reply yes or no."
+  garlic = gets.chomp
+
+  puts "Would you like to enroll in the company's health insurance? Reply yes or no."
+  health_i = gets.chomp
+
+  if employee.include?("d", "r", "a", "c", "u", "l", "a", "t" "f", "n", "g")
+    print "Definitely a vampire."
+  elsif (garlic == "no" && health_i == "no") && age > 100
+    print "Almost certainly a vampire."
+  elsif (garlic == "no" || health_i == "no") && age > 100 
+    print "Probably a vampire."
+  elsif (garlic == "yes" || health_i == "yes") && age < 100
+    print "Probably not a vampire."
+  else 
+    print "Results inconclusive."
+  end
+  puts "Your age is #{age}. You were born in #{year}. Your preference for garlic bread is #{garlic}. Your preference for health insurance is #{health_i}."
+  number_processed -= 1
+  puts "Also, please name any allergies."
+  allergy = gets.chomp! 
+  while allergy != "sunshine" 
+    if allergy == "sunshine" || allergy.include?("sunshine")
+      puts "Probably a vampire."
+      break
+    elsif allergy == "done"
+      break
+    else
+      puts "Any others?"
+      allergy = gets.chomp
+    end
+  end
+  puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
+end
+
+
+
