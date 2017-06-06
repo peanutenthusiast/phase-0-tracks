f2692166a585bd7b19f28e1c2247318978d87e26
Mark de Dios <mhdedios@gmail.com>
2017-06-06 13:57:05 -0700

    Create blank hash and pseudocode for hashes.rb

---
+++ ruby/hashes.rb
@@ -0,0 +1,27 @@
+#Ask for client name.
+#Ask for client age.
+#Converts string to number
+#Ask for number of children.
+#Asks client to describe use of room to be designed.
+#Asks client to describe what decor they see for the room.
+#Ask client to list any special needs for the room.
+#Asks client for a budget estimate.
+#Prints a summary of their information.
+#Asks client to read over this summary and decide if changes are 
+#necessary.
+#While changes are decided as necessary, repeat the questions and
+#print the new summary.
+#When the client is satisfied with the summary, stop.
+
+clientInfo = {
+  name = "",
+  age = "",
+  children_no = "",
+  room_use = "",
+  decor = "",
+  needs = "",
+  budget = "",
+}
+
+
+
