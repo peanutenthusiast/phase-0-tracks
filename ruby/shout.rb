ae46c8d9a0dba3981931e23a1ed9c4399d835890
Mark de Dios <mhdedios@gmail.com>
2017-06-17 23:05:05 -0700

    Add the hangry code to shout.rb

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,14 +1,29 @@
 module Shout
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
-  end
+  # def self.yell_angrily(words)
+  #   words + "!!!" + " :("
+  # end
 
-  def self.yelling_happily(words)
-    words + "!!!1" + " :D"
-  end 
+  # def self.yelling_happily(words)
+  #   words + "!!!1" + " :D"
+  # end 
+  def yell_hangrily(words)
+    words.upcase + "!!!!!" + ">:C"
+  end
 end
 
-p Shout.yell_angrily("All of my teeth hurt.")
+class White_G
+  include Shout
+end
 
-p Shout.yelling_happily("I've defeated capitalism.")
+class Rich_bb
+  include Shout
+end
 
+rich_bb = Rich_bb.new 
+white_g = White_G.new
+
+p rich_bb.yell_hangrily("my father ordered the foie gras burrito without beans!")
+
+p white_g.yell_hangrily("where's my starbucks?")
+
+p Math.log(5)
