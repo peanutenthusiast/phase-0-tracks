ce039fe0890204f2d30394cfa5a0bce8baa0046b
Mark de Dios <mhdedios@gmail.com>
2017-06-17 23:11:07 -0700

    Add additional method calls

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -21,9 +21,14 @@ end
 
 rich_bb = Rich_bb.new 
 white_g = White_G.new
+harris_pelton = Rich_bb.new
+money_bae_bae = White_G.new
 
 p rich_bb.yell_hangrily("my father ordered the foie gras burrito without beans!")
 
 p white_g.yell_hangrily("where's my starbucks?")
 
+p harris_pelton.yell_hangrily("That's too hot")
+
+p money_bae_bae.yell_hangrily("This has gluten?")
 p Math.log(5)
