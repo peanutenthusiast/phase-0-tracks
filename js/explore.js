1047d3729b45af0a55cf09d9c3fb49037c80f99a
Mark de Dios <mhdedios@gmail.com>
2017-06-21 23:45:56 -0700

    Do it for real

--- js/explore.js
+++ js/explore.js
@@ -1,5 +1,10 @@
 //for the length of a string, iterate from the last letter of a string to the first
 //start from index of last item, subtract 1 each time
 
+var str = "hello";
+newString = "";
 
+for (var i = str.length - 1; i >= 0; i--) {
+  newString += str[i];
+}
 
