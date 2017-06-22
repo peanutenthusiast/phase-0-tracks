271b4dd7eeb0780dfb585694a27284a3afbbbc5c
Mark de Dios <mhdedios@gmail.com>
2017-06-21 23:54:25 -0700

    Do the function thing

--- js/explore.js
+++ js/explore.js
@@ -1,10 +1,15 @@
 //for the length of a string, iterate from the last letter of a string to the first
 //start from index of last item, subtract 1 each time
 
-var str = "hello";
-newString = "";
+//var str = "hello";
 
-for (var i = str.length - 1; i >= 0; i--) {
-  newString += str[i];
+function reverseString(str) {
+  var newString = "";
+
+  for (var i = str.length - 1; i >= 0; i--) {
+    newString += str[i];
+  }
+  return newString; 
 }
 
+reverseString('palindrome');
