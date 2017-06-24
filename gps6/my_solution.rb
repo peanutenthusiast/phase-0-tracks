bc2db23d1559c543544514020795fc9c762f350d
Mark de Dios <mhdedios@gmail.com>
2017-06-23 20:18:23 -0700

    Change all the local variables to instance variables, etc.

--- gps6/my_solution.rb
+++ gps6/my_solution.rb
@@ -17,6 +17,8 @@ class VirusPredictor
     @state = state_of_origin
     @population = population
     @population_density = population_density
+    @speed = 0.0
+    @@number_of_deaths = ""
   end
 
   #calling the methods of predicted deaths and speed_of_spread
@@ -33,18 +35,18 @@ class VirusPredictor
   def predicted_deaths(population_density, population, state)
     # predicted deaths is solely based on population density
     if @population_density >= 200
-      number_of_deaths = (@population * 0.4).floor
+      @number_of_deaths = (@population * 0.4).floor
     elsif @population_density >= 150
-      number_of_deaths = (@population * 0.3).floor
+      @number_of_deaths = (@population * 0.3).floor
     elsif @population_density >= 100
-      number_of_deaths = (@population * 0.2).floor
+      @number_of_deaths = (@population * 0.2).floor
     elsif @population_density >= 50
-      number_of_deaths = (@population * 0.1).floor
+      @number_of_deaths = (@population * 0.1).floor
     else
-      number_of_deaths = (@population * 0.05).floor
+      @number_of_deaths = (@population * 0.05).floor
     end
 
-    print "#{@state} will lose #{number_of_deaths} people in this outbreak"
+    print "#{@state} will lose #{@number_of_deaths} people in this outbreak"
 
   end
 
@@ -52,21 +54,20 @@ class VirusPredictor
   def speed_of_spread(population_density, state) #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
-    speed = 0.0
 
     if @population_density >= 200
-      speed += 0.5
+      @speed += 0.5
     elsif @population_density >= 150
-      speed += 1
+      @speed += 1
     elsif @population_density >= 100
-      speed += 1.5
+      @speed += 1.5
     elsif @population_density >= 50
-      speed += 2
+      @speed += 2
     else
-      speed += 2.5
+      @speed += 2.5
     end
 
-    puts " and will spread across the state in #{speed} months.\n\n"
+    puts " and will spread across the state in #{@speed} months.\n\n"
 
   end
 
