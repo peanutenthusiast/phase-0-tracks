fac3f6cc400fa3fd6e84da6ca079f893a6840f1e
Mark de Dios <mhdedios@gmail.com>
2017-06-26 01:58:24 -0700

    Add release 2 code tentatively

--- js/algo.js
+++ js/algo.js
@@ -1,13 +1,13 @@
 function longestPhrase(arr) {
-  var longer = 0;
-  var longest;
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i].length > longer) {
-      var longer = arr[i].length;
-      longest = arr[i];
+  var longest = 0;
+  var longest_word;
+  for (var l = 0; l < arr.length; l++) {
+    if (arr[l].length > longest) {
+      longest = arr[l].length;
+      longest_word = arr[l];
     }
   }
-  console.log(longest)
+  console.log(longest_word)
 }
 
 arr = ["long phrase","longest phrase","longer phrase"]
@@ -27,7 +27,7 @@ animal2 = {animal: "Dog", legs: 3};
 function ifCommon(object1, object2) {
   var keys = Object.keys(object1);
   var keys2 = Object.keys(object2);
-  var values = Object.values(object1);
+  var values1 = Object.values(object1);
   var values2 = Object.values(object2);
   for (var i = keys.length - 1; i >= 0; i--) {
     if ((keys[i] == keys2[i] && values[i] == values2[i]) === true) {
@@ -41,13 +41,13 @@ function ifCommon(object1, object2) {
 console.log(ifCommon(person1, person2));
 console.log(ifCommon(person1, animal1));
 ifCommon(animal1, animal2);
-//call two objects as arguments
-//define keys and values as separate arrays
-//loop through the key with index
-//if the key of a certain index is equal to that the other
-//and if the value of that certain index is equal to that of the other
-//and ALL that is true, return true
-//else, return false.
+call two objects as arguments
+define keys and values as separate arrays
+loop through the key with index
+if the key of a certain index is equal to that the other
+and if the value of that certain index is equal to that of the other
+and ALL that is true, return true
+else, return false.
 
 
 //method takes an an integer for length
@@ -57,31 +57,61 @@ ifCommon(animal1, animal2);
 //returns the array
 //pops the array the longestWord function which will print the result
 
-function getRandomArbitrary(min, max) {
-  return Math.random() * (max - min) + min;
-}
+var randomInt = (Math.random() * (9) + 1)
 
 var word = ""
 var alphabet = "abcdefghijklmnopqrstuvwxyz";
 
 function randomWords() {
-  for (var i = 1; i < getRandomArbitrary(1, 10); i++) {
+  for (var i = randomInt; i > 0 ; i--) {
     word += alphabet.charAt(Math.floor(Math.random() * alphabet.length));
   }
+  return word
 }
 
 var array = [];
 
 function getRandomArraysofRandomWords(int) {
   array.length == int;
-  index = 0;
-  while (index < int) {
-    randomWords();
-    array.push(word);
-    index++;
+  for (var a = 0; a < int; a++) {
+    var item = randomWords();
+    array[a] = item
   }
-  return array
+  console.log(array);
 }
+
+getRandomArraysofRandomWords(3)
+console.log(array)
+longestPhrase(array)
+getRandomArraysofRandomWords(4)
+console.log(array)
+longestPhrase(array)
+getRandomArraysofRandomWords(6)
+console.log(array)
+longestPhrase(array)
+getRandomArraysofRandomWords(7)
+console.log(array)
+longestPhrase(array)
+console.log(array)
+getRandomArraysofRandomWords(5)
+console.log(array)
+longestPhrase(array)
+getRandomArraysofRandomWords(3)
+console.log(array)
+longestPhrase(array)
+getRandomArraysofRandomWords(4)
+console.log(array)
+longestPhrase(array)
+getRandomArraysofRandomWords(10)
+console.log(array)
+longestPhrase(array)
+getRandomArraysofRandomWords(2)
+
+
+
+
+
+
 
 
 
