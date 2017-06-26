cda2a7d13efada665446d33525d3251c7ed8ea29
Mark de Dios <mhdedios@gmail.com>
2017-06-25 20:52:57 -0700

    Add semi-working code for checking matching key value pairs between objects

--- js/algo.js
+++ js/algo.js
@@ -15,3 +15,28 @@ console.log(longestPhrase(arr));
 //Iterate through the array
 //If the length of one string is greater than the other
 //set the string of greater length equal to the longest word
+
+person1 = {name: "Steven", age: 54};
+person2 = {name: "Tamir", age: 54};
+
+animal1 = {animal: "Dog", legs: 4};
+animal2 = {animal: "Dog", legs: 3};
+
+function ifCommon(object1, object2) {
+  var keys = Object.keys(object1);
+  var keys2 = Object.keys(object2);
+  var values = Object.values(object1);
+  var values2 = Object.values(object2);
+  for (var i = keys.length - 1; i >= 0; i--) {
+    if ((keys[i] == keys2[i] && values[i] == values2[i]) === true) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+console.log(ifCommon(person1, person2));
+console.log(ifCommon(person1, animal1));
+ifCommon(animal1, animal2);
+
