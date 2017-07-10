4265cb7129f709d02533dc1d4e813f32bf9b6088
Mark de Dios <mhdedios@gmail.com>
2017-07-10 04:51:14 -0700

    Add first half of horoscopes totable

--- communist_horoscopes/communist_horoscopes.rb
+++ communist_horoscopes/communist_horoscopes.rb
@@ -16,16 +16,16 @@ db.execute(create_horoscope_table_cmd)
 
 db.execute("INSERT INTO horoscopes (horoscope) VALUES ('Conflict will arise in the workplace, and so will the outlet for your proletariat anger. Ready yourself.')")
 
-# db.execute("INSERT INTO horoscopes (horoscope) VALUES ('Your labor power finally gives way to a rise, but beware the tragedy of the commons. Do you fight for your fellow man, or do you fight for yourself? Alas, you feel the throes of individuality.
-# ')")
+db.execute("INSERT INTO horoscopes (horoscope) VALUES ('Your labor power finally gives way to a rise, but beware the tragedy of the commons. Do you fight for your fellow man, or do you fight for yourself? Alas, you feel the throes of individuality.
+')")
 
-# db.execute("INSERT INTO horoscopes (horoscope) VALUES ('If you possess belief in karma, dispossess it. No expense of capital nor labor power will return the way you expect it to.')") 
+db.execute("INSERT INTO horoscopes (horoscope) VALUES ('If you possess belief in karma, dispossess it. No expense of capital nor labor power will return the way you expect it to.')") 
 
-# db.execute("INSERT INTO horoscopes (horoscope) VALUES ('Read in the melody of “Unforgettable”: “Cancerous capitalist, that’s what you are.”')")
+db.execute("INSERT INTO horoscopes (horoscope) VALUES ('Read in the melody of “Unforgettable”: “Cancerous capitalist, that’s what you are.”')")
 
-# db.execute("INSERT INTO horoscopes (horoscope) VALUES ('Always are we told to work hard, play hard, dream big. This week, forget it. Skimp. Relax. Conserve your labor power for the revolution.')")
+db.execute("INSERT INTO horoscopes (horoscope) VALUES ('Always are we told to work hard, play hard, dream big. This week, forget it. Skimp. Relax. Conserve your labor power for the revolution.')")
 
-# db.execute("INSERT INTO horoscopes (horoscope) VALUES ('You share aims with no one. Reassess yourself.')")
+db.execute("INSERT INTO horoscopes (horoscope) VALUES ('You share aims with no one. Reassess yourself.')")
 
 # db.execute("INSERT INTO horoscopes (horoscope) VALUES ('You are target #1 in this week's capitalist agenda. Rise. Fight. Tear Big Brother asunder.')")
 
