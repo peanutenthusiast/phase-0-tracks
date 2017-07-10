88c726189399506dedb31f5921ff36e642b32a60
Mark de Dios <mhdedios@gmail.com>
2017-07-10 04:35:56 -0700

    add the file with the written syntax

--- communist_horoscopes/communist_horoscopes.rb
+++ communist_horoscopes/communist_horoscopes.rb
@@ -4,3 +4,11 @@ require 'sqlite3'
 require 'zodiac'
 
 db = SQLite3::Database.new("communist_horoscopes.db")
+
+create_horoscope_table_cmd = <<-SQL_H
+  CREATE TABLE IF NOT EXISTS horoscopes(
+    id INTEGER PRIMARY KEY,
+    horoscope TEXT
+    )
+SQL_H
+
