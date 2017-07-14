46c7dc83063ee4eac5e8666980a1132918100560
Mark de Dios <mhdedios@gmail.com>
2017-07-13 17:17:19 -0700

    Add /great_job route and /contact route for app.rb

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -44,3 +44,16 @@ get '/students/:id' do
   student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
   student.to_s
 end
+
+get '/contact' do
+  "667 Dark Avenue"
+end
+
+get '/good_job' do
+  name = params[:name]
+  if name
+    "Good job, #{name}."
+  else
+    "Good job."
+  end
+end
