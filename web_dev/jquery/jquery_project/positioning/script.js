d3d096b9faf83b4275a8534c9a7b6c2bf49dce14
Mark de Dios <mhdedios@gmail.com>
2017-07-16 23:59:54 -0700

    Add playful effects with jquery

--- web_dev/jquery/jquery_project/positioning/script.js
+++ web_dev/jquery/jquery_project/positioning/script.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
-  $("#octopus").hide('slow');
-};
+  
+  $('#octopus').click(function() {
+  $('#octopus').css({fill: "#ff0000"});
+  $('h1').html('<p>what if i clicked you?</p>')
+  $('h1').fadeOut('slow');
+  $('#octopus').fadeOut('slow');
+  });
+
+  $('#hat, #glasses').draggable();
+});
